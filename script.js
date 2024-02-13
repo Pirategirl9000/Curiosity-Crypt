a3cbe90d3d8d12432f3c6ee2397c60f2b50fb818
Pirategirl9000 <136397110+Pirategirl9000@users.noreply.github.com>
2024-02-13 19:45:01 +0000

    Added formula for level growth

--- script.js
+++ script.js
@@ -20,19 +20,49 @@ function getPossibleEnemies() {
 }
 
 function update() {
+    if (player.hp <= 0) {
+        gameOver();
+    }
 
+    if (enemy.hp <= 0) {
+        player.gold += enemy.gold;
+        player.xp += enemy.xp;
 
+    }
+
+}
+
+function gameOver() {
+    
 }
 
 class Player {
     constructor() {
         this.maxHP = 10;
+        this.xpReq = 5;
+        this.xp = 0;
         this.HP = 10;
         this.attack = 3;
         this.gold = 0;
-        this.lvl = 1;
+        this.lvl = 0;
     }
 
+    rollAttack() {
+        let roll = Math.floor(Math.random() * (20 - 1 + 1) + 1);
+        if (roll >= 15) {return this.attack * 2;} //critical hit 
+        else if (roll > 5) {return this.attack;} //basic hit
+        else {return 0;} //miss
+    }
+
+    levelUp() {
+        //Uses formula y=5 * 2^x
+        this.maxHP *= 1.5
+        this.HP = this.maxHP;
+        this.lvl++;
+
+        this.xp -= this.xpReq;
+        this.xpReq = Math.floor(5 * Math.pow(2, this.lvl))
+    }
 } const player = new Player();
 
 class Basic_Goblin {
@@ -53,6 +83,59 @@ class Basic_Goblin {
     }
 }
 
+class Beserk_Goblin {
+    constructor() {
+        this.maxHP = 25;
+        this.hp = 25;
+        this.attack = 5;
+        this.gold = 20;
+        this.xp = 100;
+        this.image = "Curiosity_Crypt_Beserk_Goblin.png";
+    }
+
+    rollAttack() {
+        let roll = Math.floor(Math.random() * (20 - 1 + 1) + 1);
+        if (roll >= 15) {return this.attack * 2;} //critical hit 
+        else if (roll > 5) {return this.attack;} //basic hit
+        else {return 0;} //miss
+    }
+}
+
+class Mage_Goblin {
+    constructor() {
+        this.maxHP = 10;
+        this.hp = 10;
+        this.attack = 4;
+        this.gold = 5;
+        this.xp = 25;
+        this.image = "Curiosity_Crypt_Mage_Goblin.png";
+    }
+    rollAttack() {
+        let roll = Math.floor(Math.random() * (20 - 1 + 1) + 1);
+        if (roll >= 15) {return this.attack * 2;} //critical hit 
+        else if (roll > 5) {return this.attack;} //basic hit
+        else {return 0;} //miss
+    }
+}
+
+class Kluthulu {
+    constructor() {
+        this.maxHP = 1000;
+        this.hp = 1000;
+        this.attack = 50;
+        this.gold = 10000;
+        this.xp = 10000;
+        this.image = "Curiosity_Crypt_Kluthulu.png";
+    }
+    rollAttack() {
+        let roll = Math.floor(Math.random() * (20 - 1 + 1) + 1);
+        if (roll >= 15) {return this.attack * 2;} //critical hit 
+        else if (roll > 5) {return this.attack;} //basic hit
+        else {return 0;} //miss
+    }
+}
+
+
 //Fullscreen Handler
 document.addEventListener("keypress", (e) => {
     if (e.key == 'f' && !fsc) {
@@ -63,3 +146,5 @@ document.addEventListener("keypress", (e) => {
         fsc = false;
     }
 });
+
+//return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
