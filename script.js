8ebb62bada99c4e26e8926a8e0c0728aab21283d
Pirategirl9000 <136397110+Pirategirl9000@users.noreply.github.com>
2024-02-13 16:41:39 +0000

    Created basic startup

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-alert("press 'f' to toggle fullscreen");
+// alert("press 'f' to toggle fullscreen");
 let fsc = false;
-let enemy;
-
-document.addEventListener("keypress", (e) => {
-    if (e.key == 'f' && !fsc) {
-        document.querySelector("body").requestFullscreen();
-        fsc = true;
-    } else {
-        document.exitFullscreen();
-        fsc = false;
-    }
-});
+let enemy = new Basic_Goblin();
+update();
 
 function spawnEnemy() {
     let possibilites = getPossibleEnemies();
@@ -28,9 +19,15 @@ function getPossibleEnemies() {
     return arr;
 }
 
+function update() {
+
+
+}
+
 class Player {
     constructor() {
-        this.hp = 10;
+        this.maxHP = 10;
+        this.HP = 10;
         this.attack = 3;
         this.gold = 0;
         this.lvl = 1;
@@ -40,13 +37,14 @@ class Player {
 
 class Basic_Goblin {
     constructor() {
+        this.maxHP = 5;
         this.hp = 5;
         this.attack = 1;
         this.gold = 1;
         this.xp = 5;
         this.image = "Curiosity_Crypt_Basic_Goblin.png";
     }
-
+    
     rollAttack() {
         let roll = Math.floor(Math.random() * (20 - 1 + 1) + 1);
         if (roll >= 15) {return this.attack * 2;} //critical hit 
@@ -55,3 +53,13 @@ class Basic_Goblin {
     }
 }
 
+//Fullscreen Handler
+document.addEventListener("keypress", (e) => {
+    if (e.key == 'f' && !fsc) {
+        document.querySelector("body").requestFullscreen();
+        fsc = true;
+    } else {
+        document.exitFullscreen();
+        fsc = false;
+    }
+});
